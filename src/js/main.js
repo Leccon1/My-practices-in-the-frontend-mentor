1c73f3efdd5ebd445d6564f6a1842da271a5a808
Leccon1 <dlavania703@gmail.com>
2025-07-09 00:45:25 +0300

    add visible or hidden for preview is js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,20 @@
-const btnShare = document.getElementById('btnShare')
+const btnShare = document.querySelector('.card__author-share')
+const cardPreview = document.querySelector('.card__preview')
 
+function togglePreview() {
+	cardPreview.classList.toggle('active')
+}
+
+if (window.innerWidth <= 768) {
+	btnShare.addEventListener('click', togglePreview)
+} else {
+	btnShare.addEventListener('mouseenter', () => {
+		cardPreview.classList.add('active')
+	})
+	btnShare.addEventListener('mouseleave', () => {
+		cardPreview.classList.remove('active')
+	})
+	cardPreview.addEventListener('mouseleave', () => {
+		cardPreview.classList.remove('active')
+	})
+}
