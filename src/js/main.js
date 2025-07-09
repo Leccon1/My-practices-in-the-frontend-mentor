b217c890a3ce0ab2af7183ed5a5ea0040dbdfe37
Leccon1 <dlavania703@gmail.com>
2025-07-10 01:07:26 +0300

    finish project layout and interactivity

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const btnShare = document.querySelector('.card__author-share')
 const cardPreview = document.querySelector('.card__preview')
-const wrapper = document.querySelector('.card__share-wrapper')
+/* const wrapper = document.getElementById('previewBlock') */
+const author = document.getElementById('authorBlock')
+const closeBtn = document.getElementById('btnClosePreview')
 
 function togglePreview() {
 	cardPreview.classList.toggle('active')
@@ -8,11 +10,28 @@ function togglePreview() {
 
 if (window.innerWidth <= 768) {
 	btnShare.addEventListener('click', togglePreview)
+	if (btnShare.classList.toString('active')) {
+		btnShare.classList.remove('active')
+	}
 } else {
-	wrapper.addEventListener('mouseenter', () => {
+	btnShare.addEventListener('mouseenter', () => {
 		cardPreview.classList.add('active')
 	})
-	wrapper.addEventListener('mouseleave', () => {
+	btnShare.addEventListener('mouseleave', () => {
 		cardPreview.classList.remove('active')
 	})
 }
+
+btnShare.addEventListener('click', () => {
+	if (window.innerWidth <= 768) {
+		author.style.display = 'none'
+		cardPreview.classList.add('active')
+	}
+})
+
+closeBtn.addEventListener('click', () => {
+	if (window.innerWidth <= 768) {
+		cardPreview.classList.remove('active')
+		author.style.display = 'flex'
+	}
+})
