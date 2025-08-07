bd72b4cfc4147cfdbadd2b5f29940bc66c1deafc
Leccon1 <dlavania703@gmail.com>
2025-08-07 23:04:07 +0300

    Update vite.config.js

--- junior/time-tracking-dashboard-main/vite.config.js
+++ junior/time-tracking-dashboard-main/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+	base: 'My-practices-in-the-frontend-mentor/junior/time-tracking-dashboard-main/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
