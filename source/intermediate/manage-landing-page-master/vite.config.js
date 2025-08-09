961b5c21a3618f8e46aa45ec93f3c16c843dc205
Leccon1 <dlavania703@gmail.com>
2025-08-09 23:51:51 +0300

    Update manage-landing-page-master source and demo

--- source/intermediate/manage-landing-page-master/vite.config.js
+++ source/intermediate/manage-landing-page-master/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-	base: 'https://github.com/Leccon1/Manage-landing-page',
+	base: 'My-practices-in-the-frontend-mentor/demo/intermediate/Manage-landing-page-demo',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
+			'@style': path.resolve(__dirname, './src/style'),
 		},
 	},
 })
