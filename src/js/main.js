9212cfe38a82f213de615f4fabdfc1bb96865092
Leccon1 <dlavania703@gamil.com>
2025-08-26 22:46:37 +0300

    burger button and panel is ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 import '/src/style/sass/main.scss'
 
-console.log('hi')
+const nav = document.querySelector('.nav')
+const burger = document.querySelector('.burger')
+
+burger.addEventListener('click', () => {
+	nav.classList.toggle('active')
+	burger.classList.toggle('active')
+})
