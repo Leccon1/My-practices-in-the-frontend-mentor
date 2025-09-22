755ee24e7a9fdb0cce453871dd24100864e24151
Leccon1 <dlavania703@gamil.com>
2025-09-22 21:16:04 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import path, { resolve } from 'path' // нужен для build rollup
+
+export default defineConfig({
+	base: 'My-practices-in-the-frontend-mentor/demo/junior/news-homepage-main',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@styles': path.resolve(__dirname, './src/style/sass'),
+			'@js': path.resolve(__dirname, './src/js'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@images': path.resolve(__dirname, './src/assets/images'),
+			'@fonts': path.resolve(__dirname, './src/assets/fonts'),
+		},
+	},
+})
