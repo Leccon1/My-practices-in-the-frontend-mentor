85cbf37d6d857db6f7a1a25518780254de3b3af2
Leccon1 <dlavania703@gamil.com>
2025-09-28 22:52:06 +0300

    add function change counter

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import '../sass/main.scss'
 const mainImg = document.querySelector('[data-main-img')
 const thumbnails = document.querySelectorAll('.gallery__thumbnails button')
 
+// Для смены класса у миниатюры
 thumbnails.forEach(thumbnail => {
 	thumbnail.addEventListener('click', () => {
 		thumbnails.forEach(thumbnail => {
@@ -12,4 +13,17 @@ thumbnails.forEach(thumbnail => {
 
 		mainImg.src = thumbnail.dataset.mainImg
 	})
+})
+
+const plusBtn = document.querySelector('[data-mount-plus]')
+const minusBtn = document.querySelector('[data-mount-minus]')
+const mountValue = document.querySelector('[data-mount-value ]')
+
+plusBtn.addEventListener('click', () => {
+	mountValue.textContent = Number(mountValue.textContent) + 1
+})
+minusBtn.addEventListener('click', () => {
+	if (Number(mountValue.textContent) > 0) {
+		mountValue.textContent = Number(mountValue.textContent) - 1
+	}
 })
