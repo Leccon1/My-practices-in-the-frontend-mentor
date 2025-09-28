1f452289c1c068aeca66dd8349568fc59a07006c
Leccon1 <dlavania703@gamil.com>
2025-09-28 22:40:31 +0300

    Added a function to change the state of a thumbnail class

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 import '../sass/main.scss'
+
+const mainImg = document.querySelector('[data-main-img')
+const thumbnails = document.querySelectorAll('.gallery__thumbnails button')
+
+thumbnails.forEach(thumbnail => {
+	thumbnail.addEventListener('click', () => {
+		thumbnails.forEach(thumbnail => {
+			thumbnail.classList.remove('gallery__thumbnails-item--current')
+		})
+		thumbnail.classList.add('gallery__thumbnails-item--current')
+
+		mainImg.src = thumbnail.dataset.mainImg
+	})
+})
