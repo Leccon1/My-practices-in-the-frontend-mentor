e450c1f17b00974cc46c392e641a2f1f4f0f6a65
Leccon1 <dlavania703@gamil.com>
2025-10-01 00:02:33 +0300

    add state visible for modal window

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,74 @@
 import '../sass/main.scss'
 
-const mainImages = document.querySelectorAll('[data-main-img]')
-const thumbnails = document.querySelectorAll('.gallery__thumbnails button')
+const productGallery = document.querySelector('.product__gallery')
+const mainImagePage = productGallery.querySelector('[data-main-img]')
+const thumbnailsPage = productGallery.querySelectorAll(
+	'.gallery__thumbnails button'
+)
 
-// Для смены класса у миниатюры
-thumbnails.forEach(thumbnail => {
+// чтобы при открытии модального окна картина была такая же как и на странице
+let currentImagesSrc = null
+
+// Переключение миниатюр на странице
+thumbnailsPage.forEach(thumbnail => {
 	thumbnail.addEventListener('click', () => {
-		thumbnails.forEach(thumbnail => {
-			thumbnail.classList.remove('gallery__thumbnails-item--current')
-		})
+		thumbnailsPage.forEach(t =>
+			t.classList.remove('gallery__thumbnails-item--current')
+		)
 		thumbnail.classList.add('gallery__thumbnails-item--current')
+		mainImagePage.src = thumbnail.dataset.mainImgSrc
 
-		mainImages.forEach(mainImg => {
-			mainImg.src = thumbnail.dataset.mainImgSrc
-		})
+		currentImagesSrc = thumbnail.dataset.mainImgSrc
 	})
 })
 
-//  Счетчик
+const modalGallery = document.querySelector('.gallery--modal')
+const mainImageModal = modalGallery.querySelector('[data-main-img]')
+const thumbnailsModal = modalGallery.querySelectorAll(
+	'.gallery__thumbnails button'
+)
+const mainImgButton = document.querySelector('[data-main-img-button]')
+const closeToModal = document.querySelector('[data-modal-closed-button]')
+const modalWindow = document.querySelector('[data-modal]')
+
+// Переключение миниатюр в модальном окне
+thumbnailsModal.forEach(thumbnail => {
+	thumbnail.addEventListener('click', () => {
+		thumbnailsModal.forEach(t =>
+			t.classList.remove('gallery__thumbnails-item--current')
+		)
+		thumbnail.classList.add('gallery__thumbnails-item--current')
+		mainImageModal.src = thumbnail.dataset.mainImgSrc
+	})
+})
+
+// Открытие модального окна
+mainImgButton.addEventListener('click', () => {
+	modalWindow.classList.add('modal--visible')
+
+	mainImageModal.src = currentImagesSrc
+})
+
+// Закрытие модального окна
+closeToModal.addEventListener('click', () => {
+	modalWindow.classList.remove('modal--visible')
+})
+
 const plusBtn = document.querySelector('[data-mount-plus]')
 const minusBtn = document.querySelector('[data-mount-minus]')
-const mountValue = document.querySelector('[data-mount-value ]')
+const mountValue = document.querySelector('[data-mount-value]')
 
 let mountCounter = 0
 
 plusBtn.addEventListener('click', () => {
 	mountCounter += 1
-	mountValue.textContent = mountCounter.toString()
+	mountValue.textContent = mountCounter
 })
+
 minusBtn.addEventListener('click', () => {
 	if (mountCounter > 0) {
 		mountCounter -= 1
-		mountValue.textContent = mountCounter.toString()
+		mountValue.textContent = mountCounter
 	}
 })
 
@@ -39,15 +76,16 @@ const addToCartButton = document.querySelector('[data-add-cart]')
 const cartBtn = document.querySelector('.cart-icon')
 const cart = document.querySelector('.cart-dropdown')
 
+// Элементы, при клике на которые корзина не закрывается
 const exceptions = ['.cart-icon', '.cart-dropdown', '.product__add']
 
-// закрыть корзину при клике вне ней
+// Открытие и закрытие корзинки при клике на кнопку
 cartBtn.addEventListener('click', e => {
 	e.stopPropagation()
-	cart.classList.toggle('cart-dropdown--visible')
+	toggleCart()
 })
 
-// при клике на определенные элементы не закрывать корзину
+// Закрытие корзины при клике вне неё
 document.addEventListener('click', e => {
 	const isException = exceptions.some(sel => e.target.closest(sel))
 	if (!isException) {
@@ -55,41 +93,54 @@ document.addEventListener('click', e => {
 	}
 })
 
+// Добавление товара в корзину
 addToCartButton.addEventListener('click', () => {
 	if (mountCounter > 0) {
 		addToCart()
 	}
 })
 
+// Добавление товара в корзину (логикаDDDDD)
 function addToCart() {
 	cart.classList.add('cart-dropdown--visible')
 	const dropdownContent = cart.querySelector('.cart-dropdown__content')
 
-	let existingProduct = dropdownContent.querySelector('.cart-dropdown__product')
+	const existingProduct = dropdownContent.querySelector(
+		'.cart-dropdown__product'
+	)
 
 	if (existingProduct) {
+		// Обновление количества и суммы, если товар уже в корзине
 		const qtyEl = existingProduct.querySelector('.cart-product__price-example')
 		const sumEl = existingProduct.querySelector('.cart-product__price-sum')
 
 		qtyEl.textContent = `$125.00 x ${mountCounter}`
 		sumEl.textContent = `$${125 * mountCounter}.00`
 	} else {
-		const product = `
-			<div class="cart-dropdown__product">
-				<img class="cart-product__img" src="./src/assets/images/image-product-1-thumbnail.jpg">
-				<div class="cart-product__info">
-					<h3 class="cart-product__name">Fall Limited Edition Sneakers</h3>
-					<div class="cart-product__price">
-						<span class="cart-product__price-example">$125.00 x ${mountCounter}</span>
-						<span class="cart-product__price-sum">$${125 * mountCounter}.00</span>
-					</div>
-				</div>
-				<button class="cart-product__delete-btn">
-					<img src="./src/assets/images/icon-delete.svg" alt="delete button">
-				</button>
-			</div>
-		`
+		// Создание нового блока товара
+		const productHTML = `
+      <div class="cart-dropdown__product">
+        <img class="cart-product__img" src="./src/assets/images/image-product-1-thumbnail.jpg" alt="product image">
+        <div class="cart-product__info">
+          <h3 class="cart-product__name">Fall Limited Edition Sneakers</h3>
+          <div class="cart-product__price">
+            <span class="cart-product__price-example">$125.00 x ${mountCounter}</span>
+            <span class="cart-product__price-sum">$${
+							125 * mountCounter
+						}.00</span>
+          </div>
+        </div>
+        <button class="cart-product__delete-btn">
+          <img src="./src/assets/images/icon-delete.svg" alt="delete button">
+        </button>
+      </div>
+    `
 
-		dropdownContent.insertAdjacentHTML('beforeend', product)
+		dropdownContent.insertAdjacentHTML('beforeend', productHTML)
 	}
+}
+
+// видно не видно корзину
+function toggleCart() {
+	cart.classList.toggle('cart-dropdown--visible')
 }
